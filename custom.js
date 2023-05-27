05e0d4c2965b32fd90e4728da4181f8b65872edc
AlHamidi123 <AlHamidi.123.manoush@gmail.com>
2023-05-28 00:44:09 +0300

    new design

--- custom.js
+++ custom.js
@@ -4,64 +4,46 @@ fetch('data.json')
     .then(data => {
         // Process the data here
         const container = document.getElementById('menu_element');
+        const container2 = document.getElementById('menu-flters');
         categories = []
         data.forEach(record => {
-            // Process each record here
+            category = record.category
+            if (category == 'Special Mankoush'){category='Special'}
+            if (!categories.includes(category)){categories.push(category)}
+        });
+        categories.forEach(categ =>{
+            const htmlCode =`<li data-filter=".filter-${categ}">${categ}</li>`
+            container2.innerHTML += htmlCode;
+        });
+        const options = { style: 'decimal' };
+        data.forEach(record => {
             category = record.category
             info = record.info
             title = info.name
-            price = info.price
+            price = parseInt(info.price).toLocaleString('en-US', options);
             dozen = info.dozen
             type = info.type
-            url = info.url 
-            // Category
-            if (!categories.includes(category)){
-                categories += [category]
-                catego_html = `
-                    <div class="container mb-2">
-                        <div class="row">
-                            <div class="col-xl-6 desktop-only"></div>
-                            <div class="col-3">
-                                <h1 class="sub_taital">${category}</h1>
-                            </div>
-                        </div>
-                    </div>
-                                `
-            }
-            else{
-                catego_html =""
-            }
+            url = info.url
+            if (category == 'Special Mankoush'){category='Special'}
             // Dozen
             if (dozen !== undefined) {
                 dozen_html = `<span>دزدينة ${dozen} .ل.ل</span>`
             } else {
                 dozen_html = ''
             }
-
             if (category !== 'Pizza') {
                 const htmlCode = `
-                ${catego_html}
-                    <div class="container mb-2">
-                        <div class="row">
-                            <div class="col-xl-3 desktop-only"></div>
-                            <div class="col-3">
-                                <img src=${url} alt="Your Image" class="image">
-                            </div>
-                            <div class="col-xl-3 col-9 d-flex align-items-center text_wrap">
-                                <div class="div text">
-                                    <p>${title}
-                                    ${price} .ل.ل
-                                    </p>
-                                    ${dozen_html}
-                                </div>
-                            </div>
-                            <div class="col-xl-3 desktop-only"></div>
-                        </div>
+                <div class="col-lg-6 menu-item filter-${category}">
+                    <div class="menu-content">
+                        <a href="#">${title}</a><span>${price} .ل.ل</span>
                     </div>
+                    <div class="menu-ingredients">
+                        ${dozen_html}
+                    </div>
+                </div>
                 `;
                 container.innerHTML += htmlCode;
             }
-           
         });
     })
     .catch (error => {
